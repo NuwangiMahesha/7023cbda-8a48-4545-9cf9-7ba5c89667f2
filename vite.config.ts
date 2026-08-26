8267a10839da982a7f653111f23626e77e7c1b3b
NuwangiMahesha <nuwangimahesha@gmail.com>
2026-08-26 10:22:49 +0530

    bug fixing

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // This ensures all routes are handled by index.html for client-side routing
+    historyApiFallback: true,
+  },
 })
